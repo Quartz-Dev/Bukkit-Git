ab5551e9ba16cc2bbdb30bea85c02ee6fb162703
rockhopper72 <stevenk72@gmail.com>
2014-12-28 17:50:23 -0500

    Download Test #1

--- src/com/quartzdev/bukkitgit/gitevent/GitDownloader.java
+++ src/com/quartzdev/bukkitgit/gitevent/GitDownloader.java
@@ -1,6 +1,10 @@
 package com.quartzdev.bukkitgit.gitevent;
 
-import org.bukkit.Bukkit;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+import net.minecraft.util.org.apache.commons.io.FileUtils;
 
 public class GitDownloader implements Runnable {
 	
@@ -12,9 +16,17 @@ public class GitDownloader implements Runnable {
 	
 	@Override
 	public void run() {
-		// TODO allow choice for master branch or default branch maybe
+		try {
+			// TODO allow choice for master branch or default branch maybe
+			
+			URL website = new URL("https://api.github.com/repos/" + event.getRepositoryFullName() + "/" + event.getCompressionType() + "/" + event.getDefaultBranch());
+			
+			File dest = new File("plugins" + File.separator + "Bukkit-Git" + File.separator + "downloads" + File.separator + "info.zip");
+			FileUtils.copyURLToFile(website, dest);
+		} catch (IOException e) {
+			// TODO Make it do something useful
+			e.printStackTrace();
+		}
 		
-		String downloadURL = "https://api.github.com/repos/" + event.getRepositoryFullName() + "/" + event.getCompressionType() + "/" + event.getDefaultBranch();
-		Bukkit.broadcastMessage("Download URL: " + downloadURL);
 	}
 }
