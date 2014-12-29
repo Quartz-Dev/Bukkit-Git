efef004ed12152e1d3419a61e6d36e2be3087d10
rockhopper72 <stevenk72@gmail.com>
2014-12-28 22:10:00 -0500

    Put it in a new method

--- src/com/quartzdev/bukkitgit/gitevent/GitDownloader.java
+++ src/com/quartzdev/bukkitgit/gitevent/GitDownloader.java
@@ -45,40 +45,7 @@ public class GitDownloader implements Runnable {
 			
 			FileUtils.copyURLToFile(website, dest);
 			
-			if (!unzippedFolder.exists()) {
-				unzippedFolder.mkdir();
-			}
-			
-			ZipInputStream zis = new ZipInputStream(new FileInputStream(dest));
-			ZipEntry ze = zis.getNextEntry();
-			
-			while (ze != null) {
-				
-				String entryName = ze.getName();
-				File newFile = new File(unzippedFolderPath + File.separator + entryName);
-				
-				System.out.println("file unzip : " + newFile.getAbsoluteFile());
-				
-				try {
-					new File(newFile.getParent()).mkdirs();
-					
-					newFile.createNewFile();
-					FileOutputStream fos = new FileOutputStream(newFile);
-					
-					int len;
-					while ((len = zis.read(buffer)) > 0) {
-						fos.write(buffer, 0, len);
-					}
-					
-					fos.close();
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-				ze = zis.getNextEntry();
-			}
-			
-			zis.closeEntry();
-			zis.close();
+			unzip(dest.getAbsolutePath(), unzippedFolder.getAbsolutePath());
 			
 			for (File file : unzippedFolder.listFiles()) {
 				if (Pattern.matches("*.java", file.getName())) {
@@ -100,5 +67,54 @@ public class GitDownloader implements Runnable {
 			e.printStackTrace();
 		}
 		
+	}
+	
+	public void unzip(String zipFile, String outputFolder) {
+		
+		byte[] buffer = new byte[1024];
+		
+		try {
+			
+			// create output directory is not exists
+			File folder = new File(outputFolder);
+			if (!folder.exists()) {
+				folder.mkdir();
+			}
+			
+			// get the zip file content
+			ZipInputStream zis = new ZipInputStream(new FileInputStream(zipFile));
+			// get the zipped file list entry
+			ZipEntry ze = zis.getNextEntry();
+			
+			while (ze != null) {
+				
+				String fileName = ze.getName();
+				File newFile = new File(outputFolder + File.separator + fileName);
+				
+				System.out.println("file unzip : " + newFile.getAbsoluteFile());
+				
+				// create all non exists folders
+				// else you will hit FileNotFoundException for compressed folder
+				new File(newFile.getParent()).mkdirs();
+				
+				FileOutputStream fos = new FileOutputStream(newFile);
+				
+				int len;
+				while ((len = zis.read(buffer)) > 0) {
+					fos.write(buffer, 0, len);
+				}
+				
+				fos.close();
+				ze = zis.getNextEntry();
+			}
+			
+			zis.closeEntry();
+			zis.close();
+			
+			System.out.println("Done");
+			
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
 	}
 }
